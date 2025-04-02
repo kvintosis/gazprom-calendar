81b9cff8f59558aa0361d186ea71251c37f8c4ec
ShunNebula <vyach1369@gmail.com>
2025-04-02 15:45:18 +0700

    # Добавлена таблица для проектов

--- calendar/src/components/Projects.vue
+++ calendar/src/components/Projects.vue
@@ -1,3 +1,83 @@
 <template>
-    hjhjkhfj
+    <div>
+    <h2>Проекты</h2>
+    <div v-if="projects.length > 0" class="container">
+        <div class="row project-header">
+        <div class="col-3 name">Название</div>
+        <div class="col-6 description">Описание</div>
+        <div class="col-3 actions"></div>
+
+        </div>
+        <div v-for="project in projects" :key="project.id" class="row project-item">
+        <div class="col-3 project-name">{{ project.name }}</div>
+        <div class="col-6 project-description">{{ project.description }}</div>
+        <div class="col-3 project-actions">
+            <button @click="showDetails(project.id)" class="btn">
+            Подробнее
+            </button>
+        </div>
+        </div>
+    </div>
+    <p v-else>Нет доступных проектов.</p>
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+    return {
+        projects: [],
+    };
+    },
+    mounted() {
+        this.loadProjects();
+    },
+    methods: {
+        async loadProjects() {
+            setTimeout(() => {
+                this.projects = [
+                    { id: 1, name: 'Тут название', description: 'Тут описание' },
+                    { id: 2, name: 'Тут название', description: 'Тут описание' },
+                ];
+            }, 200);
+        },
+        showDetails(projectId) {
+            // Что-то да будет
+        },
+    },
+};
+</script>
+
+<style scoped>
+.project-header {
+    border-top: 1px solid #ccc;
+    border-bottom: 1px solid #ccc;
+    align-items: center;
+}
+
+.project-item {
+    align-items: center;
+    margin-top: 5px;
+    margin-bottom: 5px;
+}
+
+.project-photo img {
+    background-color: #ddd;
+    object-fit: cover;
+}
+
+.project-actions {
+    text-align: right;
+}
+
+.btn {
+    width: 120px;
+    height: 30px;
+    border-radius: 30px;
+    background: #007AC2;
+    color: #FFFFFF;
+    size: 32px;
+    text-align: center;
+    padding: 5px;
+}
+</style>
